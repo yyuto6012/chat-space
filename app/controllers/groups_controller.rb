40b7ae31e89614c58209bb8eedf279aad4c85da2
yyuto6012 <(yyabu0618@gmail.com)>
2017-02-10 11:35:20 +0900

    define groups new and create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,14 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(groups_params)
+  end
+
+  private
+  def groups_params
+    params.require(:group).permit(:name)
+  end
 end
