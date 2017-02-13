e6fdcefbc3851c03d5447780dd170ad20e850709
yyuto6012 <(yyabu0618@gmail.com)>
2017-02-13 12:52:06 +0900

    modify

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+before_action :set_group, only: [:edit, :update]
 
   def new
     @group = Group.new
@@ -10,19 +11,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
-
   def update
-      group = Group.find(params[:id])
-      group.update(groups_params)
+      @group.update(groups_params)
       redirect_to controller: :top, action: :index
   end
 
   private
   def groups_params
     params.require(:group).permit(:group_name, {user_ids:[]})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
