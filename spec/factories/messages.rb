78504c8e45179ac881b33d8504357f4ac7e1d5b5
yyuto6012 <(yyabu0618@gmail.com)>
2017-02-23 15:03:20 +0900

    add variable-id

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
 
   factory :message do
-    id    Faker::Number.digit
+    sequence :id do |n|
+          n
+    end
     body 'techcamp'
     image Faker::Avatar.image
-    user_id Faker::Number.digit
-    group_id Faker::Number.digit
+    user_id 1
+    group_id 1
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
     updated_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
