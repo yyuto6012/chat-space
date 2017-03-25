68bfe994281328c50da42ba605c995020f66038b
yyuto6012 <(yyabu0618@gmail.com)>
2017-03-25 15:05:05 +0900

    modify2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'chat-space'
-set :repo_url, 'git@github.com:chat-space.git'
+set :repo_url, 'git@github.com:yyuto6012/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
